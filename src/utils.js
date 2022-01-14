e31265801d5a8e1cb4ea5bc9165dc08e7a6799d1
Oliver Nordbjerg <hi@notbjerg.me>
2022-01-15 00:37:49 +0100

    Normalize archs

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,18 @@
 const os = require('os')
 const path = require('path')
 
+function mapArch (arch) {
+  const mappings = {
+    x32: '386',
+    x64: 'amd64'
+  }
+
+  return mappings[arch] || arch
+}
+
 function getDownloadObject (version) {
   const platform = os.platform()
-  const filename = `foundry_${version}_${platform}_${os.arch()}`
+  const filename = `foundry_${version}_${platform}_${mapArch(os.arch())}`
   const extension = platform === 'win32' ? 'zip' : 'tar.gz'
   const binPath = platform === 'win32' ? 'bin' : path.join(filename, 'bin')
   const url = `https://github.com/gakonst/foundry/releases/download/${version}/${filename}.${extension}`
