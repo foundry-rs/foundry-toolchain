187050edb7e24095009f00c89099dea38d40e17b
Oliver Nordbjerg <hi@notbjerg.me>
2022-02-22 16:19:45 +0100

    chore: styling

--- src/utils.js
+++ src/utils.js
@@ -9,10 +9,13 @@ function mapArch (arch) {
   return mappings[arch] || arch
 }
 
+function normalizeVersionName (version) {
+  return version.replace(/^nightly-[0-9a-f]{40}$/, 'nightly')
+}
+
 function getDownloadObject (version) {
-  const version_name = version.replace(/^nightly-[0-9a-f]{40}$/, "nightly");
   const platform = os.platform()
-  const filename = `foundry_${version_name}_${platform}_${mapArch(os.arch())}`
+  const filename = `foundry_${normalizeVersionName(version)}_${platform}_${mapArch(os.arch())}`
   const extension = platform === 'win32' ? 'zip' : 'tar.gz'
   const url = `https://github.com/gakonst/foundry/releases/download/${version}/${filename}.${extension}`
 
