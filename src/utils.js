b75f3779556ded669762ab699342330bc726b176
Igor Żuk <igor.zuk@protonmail.com>
2022-02-22 10:05:52 +0100

    Support a specific nightly release

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,4 @@
 const os = require('os')
-const path = require('path')
 
 function mapArch (arch) {
   const mappings = {
@@ -11,8 +10,9 @@ function mapArch (arch) {
 }
 
 function getDownloadObject (version) {
+  const version_name = version.replace(/^nightly-[0-9a-f]{40}$/, "nightly");
   const platform = os.platform()
-  const filename = `foundry_${version}_${platform}_${mapArch(os.arch())}`
+  const filename = `foundry_${version_name}_${platform}_${mapArch(os.arch())}`
   const extension = platform === 'win32' ? 'zip' : 'tar.gz'
   const url = `https://github.com/gakonst/foundry/releases/download/${version}/${filename}.${extension}`
 
