b19171e7bf442f3ed2d102bbfe97750906f261be
Pascal Marco Caversaccio <pcaversaccio@users.noreply.github.com>
2022-10-31 16:39:33 +0000

    fix outdated link (#14)
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>
    
    Signed-off-by: Pascal Marco Caversaccio <pascal.caversaccio@hotmail.ch>

--- dist/index.js
+++ dist/index.js
@@ -5765,7 +5765,7 @@ function getDownloadObject (version) {
   const platform = os.platform()
   const filename = `foundry_${normalizeVersionName(version)}_${platform}_${mapArch(os.arch())}`
   const extension = platform === 'win32' ? 'zip' : 'tar.gz'
-  const url = `https://github.com/gakonst/foundry/releases/download/${version}/${filename}.${extension}`
+  const url = `https://github.com/foundry-rs/foundry/releases/download/${version}/${filename}.${extension}`
 
   return {
     url,
